f05e552d68f14c4a8ec632f21f49d12d7731506b
DucToanDev <pductoandev@gmail.com>
2025-12-01 15:04:37 +0700

    update sidebar

---
+++ frontend/src/modules/admin/components/sidebar.js
@@ -0,0 +1,5 @@
+const menuItems = [
+    {name: 'Dashboard', link: '/admin/dashboard'},
+    {name: 'Quản lý danh mục', link: '/admin/categories'},
+    {name: 'Quản lý sản phẩm', link: '/admin/products'},
+];
